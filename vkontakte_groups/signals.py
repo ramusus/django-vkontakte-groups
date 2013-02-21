fd9f59aafc72b382ee048298aec4360fa4faacfc
ramusus <ramusus@gmail.com>
2013-02-21 11:16:30 +0530

    remove celery dependancy

--- vkontakte_groups/signals.py
+++ vkontakte_groups/signals.py
@@ -3,13 +3,13 @@ from django.dispatch import Signal
 from django.conf import settings
 from annoying.decorators import signals
 from models import Group
-from tasks import VkontakteGroupUpdateUsersM2M
 
 group_users_updated = Signal(providing_args=['instance', 'ids'])
 
 @signals(group_users_updated, sender=Group)
 def group_users_update_m2m(sender, instance, **kwargs):
     if 'djcelery' in settings.INSTALLED_APPS:
+        from tasks import VkontakteGroupUpdateUsersM2M
         return VkontakteGroupUpdateUsersM2M.delay(instance)
     else:
         instance.update_users_m2m()
