244ba089125f675c877ba8cfd69347e4e658c455
ramusus <ramusus@gmail.com>
2013-11-13 13:06:01 +0530

    added index to column fetched

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
     install_requires=[
-        'django-vkontakte-api>=0.3.13',
+        'django-vkontakte-api>=0.4.5',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
