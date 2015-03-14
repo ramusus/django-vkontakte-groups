f604bfe54dde9c8366425e89e863089f350c63a9
ramusus <ramusus@gmail.com>
2015-03-14 14:08:45 +0700

    migration and sql

--- vkontakte_groups/sql/group.postgresql_psycopg2.sql
+++ vkontakte_groups/sql/group.postgresql_psycopg2.sql
@@ -1,16 +1,26 @@
 --vkontakte_groups_group_members
 
-CREATE UNIQUE INDEX vkontakte_groups_group_members_time_from_3col_uniq ON vkontakte_groups_group_members (group_id, user_id, time_from)
+CREATE UNIQUE INDEX vkontakte_groups_group_members_time_from_3col_uniq
+ON vkontakte_groups_group_members (group_id, user_id, time_from)
 WHERE time_from IS NOT NULL;
 
-
-CREATE UNIQUE INDEX vkontakte_groups_group_members_time_from_2col_uniq ON vkontakte_groups_group_members (group_id, user_id)
+CREATE UNIQUE INDEX vkontakte_groups_group_members_time_from_2col_uniq
+ON vkontakte_groups_group_members (group_id, user_id)
 WHERE time_from IS NULL;
 
-
-CREATE UNIQUE INDEX vkontakte_groups_group_members_time_to_3col_uniq ON vkontakte_groups_group_members (group_id, user_id, time_to)
+CREATE UNIQUE INDEX vkontakte_groups_group_members_time_to_3col_uniq
+ON vkontakte_groups_group_members (group_id, user_id, time_to)
 WHERE time_to IS NOT NULL;
 
-
-CREATE UNIQUE INDEX vkontakte_groups_group_members_time_to_2col_uniq ON vkontakte_groups_group_members (group_id, user_id)
+CREATE UNIQUE INDEX vkontakte_groups_group_members_time_to_2col_uniq
+ON vkontakte_groups_group_members (group_id, user_id)
 WHERE time_to IS NULL;
+
+-- Foreign Key: user_id_refs_remote_id_c2a7ff93
+
+ALTER TABLE vkontakte_groups_group_members DROP CONSTRAINT user_id_refs_remote_id_c2a7ff93;
+
+--ALTER TABLE vkontakte_groups_group_members
+--  ADD CONSTRAINT user_id_refs_remote_id_c2a7ff93 FOREIGN KEY (user_id)
+--      REFERENCES vkontakte_users_user (remote_id) MATCH SIMPLE
+--      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED;
