12e94e7d4154caee1e6cf56af06fa110f28c7e99
ramusus <ramusus@gmail.com>
2015-03-20 19:30:24 +0700

    updated admin

--- vkontakte_groups/admin.py
+++ vkontakte_groups/admin.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.utils.translation import ugettext as _
 from vkontakte_api.admin import VkontakteModelAdmin
-from django import forms
 from models import Group
 
-class GroupAdmin(VkontakteModelAdmin):
 
+class GroupAdmin(VkontakteModelAdmin):
     def image_preview(self, obj):
         return u'<a href="%s"><img src="%s" height="30" /></a>' % (obj.photo_big, obj.photo)
+
     image_preview.short_description = u'Картинка'
     image_preview.allow_tags = True
 
     search_fields = ('name',)
-    list_display = ('image_preview','name','screen_name','type')
-    list_display_links = ('name','screen_name')
-    list_filter = ('type','is_closed','is_admin')
+    list_display = ('image_preview', 'name', 'screen_name', 'type')
+    list_display_links = ('name', 'screen_name')
+    list_filter = ('type', 'is_closed', 'is_admin')
 
-    exclude = ('users',)
+    exclude = ('members',)
+
 
 admin.site.register(Group, GroupAdmin)
